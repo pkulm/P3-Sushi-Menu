98ad043a697aecc6872ab8c6b32ef021727067b6
Paige Kulm <paige.kulm@eagles.ewu.edu>
2013-05-23 14:14:45 -0700

    fixed lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,5 @@ $('#da-thumbs > li').each( function() {
 	$(this).hoverdir(); 
 });
 
-
-$('#da-thumbs > li > a').avgrund({
-			height: 200,
-			holderClass: 'custom',
-			showClose: true,
-			showCloseText: 'Close',
-			enableStackAnimation: true,
-			onBlurContainer: '.container',
-			template: '<ul><li>$11.00</li><li>Contains Raw Fish</li><li>Shrip, crab stick and avocado rolled with salmon, yellowtail and tuna</li></ul>'
-		});
-
 	
 });
