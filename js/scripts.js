b61c6686251cc6822d8134cde7189df33071df8e
Kulm <pkulm@it-rpub12-x7.eastern.ewu.edu>
2013-05-22 23:19:20 -0700

    added content

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
 // JavaScript Document
 
 $(document).ready(function(){
-	//$('.nav li:first').html('<img src="img/logo.png" />');
 	
-	$('.carousel').carousel({
-  interval: 3000
-})
 
-$('#myTab a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-})
 	
 });
